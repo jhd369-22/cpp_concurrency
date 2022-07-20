28c13e55d2839584cbe4a1c35690dfea7ff3257c
Junhong Deng <dengjunhong@gmail.com>
2022-07-20 02:40:48 -0700

    chore: upload files feat: implement the queue.hpp test: add test cases for queue.hpp

---
+++ app/test_queue.cpp
@@ -0,0 +1,141 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <ra/queue.hpp>
+#include <thread>
+#include <vector>
+
+TEMPLATE_TEST_CASE("single thread basic functionality", "[ra::concurrency::queue]", int, double) {
+    namespace ra = ra::concurrency;
+    using queue_type = typename ra::queue<TestType>;
+
+    queue_type q(10);
+
+    CHECK(q.is_empty());
+    CHECK(q.max_size() == 10);
+
+    CHECK(q.push(TestType(1)) == queue_type::status::success);
+    CHECK(q.push(TestType(2)) == queue_type::status::success);
+    CHECK(q.push(TestType(3)) == queue_type::status::success);
+    CHECK(q.push(TestType(4)) == queue_type::status::success);
+    CHECK(q.push(TestType(5)) == queue_type::status::success);
+    CHECK(q.push(TestType(6)) == queue_type::status::success);
+    CHECK(q.push(TestType(7)) == queue_type::status::success);
+    CHECK(q.push(TestType(8)) == queue_type::status::success);
+    CHECK(q.push(TestType(9)) == queue_type::status::success);
+    CHECK(q.push(TestType(10)) == queue_type::status::success);
+
+    SECTION("full") {
+        CHECK(q.is_full() == true);
+    }
+
+    SECTION("closed") {
+        q.close();
+        CHECK(q.is_closed() == true);
+        CHECK(q.push(TestType(11)) == queue_type::status::closed);
+
+        queue_type::value_type x;
+        CHECK(q.pop(x) == queue_type::status::success);
+        CHECK(x == TestType(1));
+    }
+
+    SECTION("pop") {
+        queue_type::value_type x;
+        CHECK(q.pop(x) == queue_type::status::success);
+        CHECK(x == TestType(1));
+    }
+
+    SECTION("clear") {
+        q.clear();
+        CHECK(q.is_empty() == true);
+
+        q.close();
+        queue_type::value_type x;
+        CHECK(q.pop(x) == queue_type::status::closed);
+    }
+}
+
+TEMPLATE_TEST_CASE("multi thread basic functionality", "[ra::concurrency::queue]", int, double) {
+    namespace ra = ra::concurrency;
+    using queue_type = typename ra::queue<TestType>;
+
+    queue_type q(10);
+
+    SECTION("push and pop without close") {
+        std::vector<std::thread> threads;
+        for (int i = 0; i < 4; ++i) {
+            threads.push_back(std::thread([&q]() {
+                for (int j = 0; j < 10; ++j) {
+                    queue_type::value_type x;
+                    CHECK(q.pop(x) == queue_type::status::success);
+                }
+            }));
+
+            threads.push_back(std::thread([&q](int i) {
+                for (int j = 0; j < 10; ++j) {
+                    CHECK(q.push(TestType(j + i * 10)) == queue_type::status::success);
+                }
+            })(i));
+        }
+
+        for (auto& t : threads) {
+            t.join();
+        }
+
+        CHECK(q.is_empty() == true);
+        CHECK(q.max_size() == 10);
+        CHECK(q.is_full() == false);
+    }
+
+    SECTION("push with close") {
+
+        std::thread t1([&q](int i) {
+            for (int j = 0; j < 10; ++j) {
+                CHECK(q.push(TestType(j)) == queue_type::status::success);
+            }
+        });
+
+        t1.join();
+
+        std::thread t2([&q](int i) {
+            for (int j = 0; j < 10; ++j) {
+                CHECK(q.push(TestType(j)) == queue_type::status::closed);
+            }
+        });
+
+        q.close();
+
+        t2.join();
+
+        CHECK(q.is_empty() == false);
+        CHECK(q.max_size() == 10);
+        CHECK(q.is_full() == true);
+        CHECK(q.is_closed() == true);
+    }
+
+    SECTION("pop with close") {
+        std::thread t1([&q](int i) {
+            for (int j = 0; j < 10; ++j) {
+                CHECK(q.push(TestType(j)) == queue_type::status::success);
+            }
+        });
+
+        t1.join();
+
+        q.close();
+
+        std::thread t2([&q](int i) {
+            for (int j = 0; j < 10; ++j) {
+                queue_type::value_type x;
+                CHECK(q.pop(x) == queue_type::status::success);
+            }
+        });
+
+
+        t2.join();
+
+        CHECK(q.is_empty() == true);
+        CHECK(q.max_size() == 10);
+        CHECK(q.is_full() == false);
+        CHECK(q.is_closed() == true);
+    }
+}
---
+++ include/ra/queue.hpp
@@ -0,0 +1,188 @@
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+namespace ra::concurrency {
+
+    // Concurrent bounded FIFO queue class.
+    template <class T>
+    class queue {
+        public:
+            // The type of each of the elements stored in the queue.
+            using value_type = T;
+
+            // An unsigned integral type used to represent sizes.
+            using size_type = std::size_t;
+
+            // A type for the status of a queue operation.
+            enum class status {
+                success = 0,  // operation successful
+                empty,        // queue is empty (not currently used)
+                full,         // queue is full (not currently used)
+                closed,       // queue is closed
+            };
+
+            // A queue is not default constructible.
+            queue() = delete;
+
+            // Constructs a queue with a maximum size of max_size.
+            // The queue is marked as open (i.e., not closed).
+            // Precondition: The quantity max_size must be greater than
+            // zero.
+            queue(size_type max_size) : max_size_(max_size), closed_(false) {}
+
+            // A queue is not movable or copyable.
+            queue(const queue&) = delete;
+            queue& operator=(const queue&) = delete;
+            queue(queue&&) = delete;
+            queue& operator=(queue&&) = delete;
+
+            // Destroys the queue after closing the queue (if not already
+            // closed) and clearing the queue (if not already empty).
+            ~queue() {
+                if (!closed_) {
+                    close();
+                }
+
+                if (!is_empty()) {
+                    clear();
+                }
+            }
+
+            // Inserts the value x at the end of the queue, blocking if
+            // necessary.
+            // If the queue is full, the thread will be blocked until the
+            // queue insertion can be completed or the queue is closed.
+            // If the value x is successfully inserted on the queue, the
+            // function returns status::success.
+            // If the value x cannot be inserted on the queue (due to the
+            // queue being closed), the function returns with a return
+            // value of status::closed.
+            // This function is thread safe.
+            // Note: The rvalue reference parameter is intentional and
+            // implies that the push function is permitted to change
+            // the value of x (e.g., by moving from x).
+            status push(value_type&& x) {
+                std::unique_lock<std::mutex> lock(mutex_);
+
+                // Wait until the queue is not full or the queue is closed.
+                condition_.wait(lock, [this]() { return !is_full() || closed_; });
+
+                // If the queue is closed, return with status::closed.
+                if (closed_) {
+                    return status::closed;
+                }
+
+                // Insert the value x at the end of the queue.
+                queue_.push(std::move(x));
+
+                // Notify any threads waiting for the queue to be not full.
+                condition_.notify_one();
+
+                return status::success;
+            }
+
+            // Removes the value from the front of the queue and places it
+            // in x, blocking if necessary.
+            // If the queue is empty and not closed, the thread is blocked
+            // until: 1) a value can be removed from the queue; or 2) the
+            // queue is closed.
+            // If the queue is closed, the function does not block and either
+            // returns status::closed or status::success, depending on whether
+            // a value can be successfully removed from the queue.
+            // If a value is successfully removed from the queue, the value
+            // is placed in x and the function returns status::success.
+            // If a value cannot be successfully removed from the queue (due to
+            // the queue being both empty and closed), the function returns
+            // status::closed.
+            // This function is thread safe.
+            status pop(value_type& x) {
+                std::unique_lock<std::mutex> lock(mutex_);
+
+                // Wait until the queue is not empty or the queue is closed.
+                condition_.wait(lock, [this]() { return !is_empty() || closed_; });
+
+                // If the queue is empty and closed, or the queue is empty, return with status::closed.
+                if ((is_empty() && closed_) || is_empty()) {
+                    return status::closed;
+                }
+
+                // Remove the value from the front of the queue.
+                x = std::move(queue_.front());
+                queue_.pop();
+
+                // Notify any threads waiting for the queue to be not empty.
+                condition_.notify_one();
+
+                return status::success;
+            }
+
+            // Closes the queue.
+            // The queue is placed in the closed state.
+            // The closed state prevents more items from being inserted
+            // on the queue, but it does not clear the items that are
+            // already on the queue.
+            // Invoking this function on a closed queue has no effect.
+            // This function is thread safe.
+            void close() {
+                std::unique_lock<std::mutex> lock(mutex_);
+                closed_ = true;
+                condition_.notify_all();
+            }
+
+            // Clears the queue.
+            // All of the elements on the queue are discarded.
+            // This function is thread safe.
+            void clear() {
+                std::unique_lock<std::mutex> lock(mutex_);
+                while (!queue_.empty()) {
+                    queue_.pop();
+                }
+            }
+
+            // Returns if the queue is currently full (i.e., the number of
+            // elements in the queue equals the maximum queue size).
+            // This function is not thread safe.
+            bool is_full() const {
+                return queue_.size() == max_size_;
+            }
+
+            // Returns if the queue is currently empty.
+            // This function is not thread safe.
+            bool is_empty() const {
+                return queue_.empty();
+            }
+
+            // Returns if the queue is closed (i.e., in the closed state).
+            // This function is not thread safe.
+            bool is_closed() const {
+                return closed_;
+            }
+
+            // Returns the maximum number of elements that can be held in
+            // the queue.
+            // This function is not thread safe.
+            size_type max_size() const {
+                return max_size_;
+            }
+
+        private:
+
+            // The maximum number of elements that can be held in the queue.
+            size_type max_size_;
+
+            // The flag used to indicate whether the queue is closed.
+            bool closed_;
+
+            // The mutex used to protect the queue.
+            mutable std::mutex mutex_;
+
+            // The condition variable used to block threads when the queue is
+            // full or empty.
+            mutable std::condition_variable condition_;
+
+            // The queue of elements.
+            std::queue<value_type> queue_;
+    };
+
+}  // namespace ra::concurrency
