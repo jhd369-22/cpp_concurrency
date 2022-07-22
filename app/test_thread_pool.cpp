a1266ea91e6eb33cfdf2c14c81162d94a0628ddb
Junhong Deng <dengjunhong@gmail.com>
2022-07-22 14:57:53 -0700

    test: update test case; use 10 threads to schedule 10000 tasks, each thread 1000 tasks

--- app/test_thread_pool.cpp
+++ app/test_thread_pool.cpp
@@ -6,15 +6,24 @@ TEST_CASE("counter increment", "[thread_pool]") {
     namespace rc = ra::concurrency;
     rc::thread_pool::size_type counter = 0;
     rc::thread_pool pool(10);
+    std::vector<std::thread> threads;
 
-    for (int i = 0; i < 100; ++i) {
-        pool.schedule(std::function<void()>([&counter]() {
-            ++counter;
+    for (int i = 0; i < 10; ++i) {
+        threads.emplace_back(std::thread([&pool, &counter]() {
+            for (int i = 0; i < 1000; ++i) {
+                pool.schedule(std::function<void()>([&counter]() {
+                    ++counter;
+                }));
+            }
         }));
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
     }
 
     CHECK(pool.size() == 10);
     pool.shutdown();
     CHECK(pool.is_shutdown() == true);
-    CHECK(counter == 100);
+    CHECK(counter == 10000);
 }
